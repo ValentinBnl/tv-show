c19bc5ce1fd88138472762991a4b48db4bef265f
Valentin Bunel <bunel.valentin@hotmail.fr>
2025-06-11 17:02:58 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+	return (
+		<div className="main_container">
+			<div className="header">
+				<input type="text" />
+			</div>
+			<div className="details"></div>
+			<div className="recommendations"></div>
+		</div>
+	);
+}
+
+export { App };
