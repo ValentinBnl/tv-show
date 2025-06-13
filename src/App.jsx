589d6fb1aa37c3f8490e313b85bb7c6c0ec47249
Valentin Bunel <bunel.valentin@hotmail.fr>
2025-06-13 11:55:41 +0200

    chore(api): load API key from .env file instead of hardcoding

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
+import { useState, useEffect } from "react";
 import { TVShowAPI } from "./api/tv-show";
 import s from "./style.module.css";
 
-TVShowAPI.fetchPopulars();
-function App() {
+export function App() {
+	const [currentTvShow, setCurrentTVShow] = useState();
+
+	async function fetchPopulars() {
+		const populars = await TVShowAPI.fetchPopulars();
+		if (populars.length > 0) {
+			setCurrentTVShow(populars[0]);
+		}
+	}
+
+	useEffect(() => {
+		async function fetchData() {
+			await fetchPopulars();
+		}
+		fetchData();
+	}, []);
+
+	console.log("***", currentTvShow);
+
 	return (
-		<div className={s.main_container}>
+		<div
+			className={s.main_container}
+			style={{
+				background: currentTvShow
+					? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url(https://image.tmdb.org/t/p/original${currentTvShow.backdrop_path}) no-repeat center / cover`
+					: "black",
+			}}
+		>
 			<div className={s.header}>
 				<div className="row">
 					<div className="col-4">
 						<div>Logo</div>
-						<div>Subtittle</div>
+						<div>Subtitle</div>
 					</div>
 					<div className="col-sm-12 col-md-4">
 						<input style={{ width: "100%" }} type="text" />
 					</div>
 				</div>
 			</div>
-			<div className={s.tv_show_details}>Detail</div>
+			<div className={s.tv_show_details}>Detail :</div>
 			<div className={s.recommendations}>Recommendations</div>
 		</div>
 	);
 }
-
-export { App };
--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,11 +1,13 @@
 import axios from "axios";
-const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=923a26eaf56d062ecaa4c411a51ddfa0";
+import { BASE_URL } from "../config";
 
 export class TVShowAPI {
 	static async fetchPopulars() {
-		const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
-		console.log("***", response.data.results);
+		const API_KEY = import.meta.env.VITE_API_KEY_PARAM;
+
+		const response = await axios.get(
+			`${BASE_URL}tv/popular?api_key=${API_KEY}`
+		);
 
 		return response.data.results;
 	}
